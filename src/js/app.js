7beb3608a46bc4914c3608816f96069ee03fff2f
Adam Thompson <adam@adamth.com>
2017-08-29 12:49:20 +0200

    Update main component

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './components/gameboard.jsx';
+import Main from './components/main.jsx';
 
 ReactDOM.render(
     <Main />,
